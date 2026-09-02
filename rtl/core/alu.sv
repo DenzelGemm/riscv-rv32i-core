ce305d7bb4f684bfa0d9343e5d89c1e7750ab1ba
DenzelGemm <girnykdanylo@gmail.com>
2026-09-02 20:56:12 +0300

    alu : add ALU_PASS_B

--- rtl/core/alu.sv
+++ rtl/core/alu.sv
@@ -6,35 +6,37 @@ module alu (
     output logic        zero
 );
 
-typedef enum logic [3:0] {
-    ALU_ADD  = 4'b0000,
-    ALU_SUB  = 4'b1000,
-    ALU_SLL  = 4'b0001,
-    ALU_SLT  = 4'b0010,
-    ALU_SLTU = 4'b0011,
-    ALU_XOR  = 4'b0100,
-    ALU_SRL  = 4'b0101,
-    ALU_SRA  = 4'b1100,
-    ALU_OR   = 4'b0110,
-    ALU_AND  = 4'b0111
-} alu_op_t;
+    typedef enum logic [3:0] {
+        ALU_ADD    = 4'b0000,
+        ALU_SUB    = 4'b1000,
+        ALU_SLL    = 4'b0001,
+        ALU_SLT    = 4'b0010,
+        ALU_SLTU   = 4'b0011,
+        ALU_XOR    = 4'b0100,
+        ALU_SRL    = 4'b0101,
+        ALU_SRA    = 4'b1100,
+        ALU_OR     = 4'b0110,
+        ALU_AND    = 4'b0111,
+        ALU_PASS_B = 4'b1111
+    } alu_op_t;
 
-always_comb begin
-    case (op)
-        ALU_ADD : result = a + b;
-        ALU_SUB : result = a - b;
-        ALU_SLL : result = a << b[4:0];
-        ALU_SLT : result = ($signed(a) < $signed(b)) ? 32'd1 : 32'd0;
-        ALU_SLTU: result = (a < b) ? 32'd1 : 32'd0;
-        ALU_XOR : result = a ^ b;
-        ALU_SRL : result = a >> b[4:0];
-        ALU_SRA : result = $signed(a) >>> b[4:0];
-        ALU_OR  : result = a | b;
-        ALU_AND : result = a & b;
-        default: result = 32'd0;
-    endcase
-end
+    always_comb begin
+        case (op)
+            ALU_ADD   : result = a + b;
+            ALU_SUB   : result = a - b;
+            ALU_SLL   : result = a << b[4:0];
+            ALU_SLT   : result = ($signed(a) < $signed(b)) ? 32'd1 : 32'd0;
+            ALU_SLTU  : result = (a < b) ? 32'd1 : 32'd0;
+            ALU_XOR   : result = a ^ b;
+            ALU_SRL   : result = a >> b[4:0];
+            ALU_SRA   : result = $signed(a) >>> b[4:0];
+            ALU_OR    : result = a | b;
+            ALU_AND   : result = a & b;
+            ALU_PASS_B: result = b;
+            default   : result = 32'd0;
+        endcase
+    end
 
-assign zero = (result == 32'd0);
+    assign zero = (result == 32'd0);
 
 endmodule
